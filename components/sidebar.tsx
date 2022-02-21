b635b8b92069128e03387444ba0c6fbe1f20a77f
青塚琢也 <takuya.aotsuka@aotsukatakuyanoMacBook-Air.local>
2022-02-21 21:44:21 +0900

    サイドバーのリンクを有効にした

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Box, List, ListItem } from "@chakra-ui/react";
+import { Box, Link, List, ListItem } from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
 
 const Sidebar: React.VFC = () => {
@@ -10,15 +11,40 @@ const Sidebar: React.VFC = () => {
       <List p="4" cursor="pointer">
         <ListItem>Article</ListItem>
         <List px="3" mb="4">
-          <ListItem>Index</ListItem>
-          <ListItem>New</ListItem>
+          <NextLink href="/article" passHref>
+            <Link>
+              <ListItem>Index</ListItem>
+            </Link>
+          </NextLink>
+          <NextLink href="/article/store" passHref>
+            <Link>
+              <ListItem>New</ListItem>
+            </Link>
+          </NextLink>
         </List>
         <ListItem>Category</ListItem>
         <List px="3" mb="4">
-          <ListItem>Index</ListItem>
-          <ListItem>New</ListItem>
+          <NextLink href="/category" passHref>
+            <Link>
+              <ListItem>Index</ListItem>
+            </Link>
+          </NextLink>
+          <NextLink href="/category/store" passHref>
+            <Link>
+              <ListItem>New</ListItem>
+            </Link>
+          </NextLink>
         </List>
-        <ListItem onClick={() => logout({returnTo: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URL_AFTER_LOGOUT!})}>Logout</ListItem>
+        <ListItem
+          onClick={() =>
+            logout({
+              returnTo:
+                process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URL_AFTER_LOGOUT!,
+            })
+          }
+        >
+          Logout
+        </ListItem>
       </List>
     </Box>
   );
