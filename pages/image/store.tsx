d799cfc0b7cfbe968510b986e4673aa10a13fe4b
青塚琢也 <takuya.aotsuka@aotsukatakuyanoMacBook-Air.local>
2022-02-22 19:35:55 +0900

    画像の新規登録を実装

--- pages/image/store.tsx
+++ pages/image/store.tsx
@@ -1,4 +1,4 @@
-import { Box, Input, Link } from "@chakra-ui/react";
+import { Box, Image, Input, Link, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useState } from "react";
 import { postApi } from "../../api/api";
@@ -11,7 +11,11 @@ const Store: React.VFC = () => {
   const uploadImage = async (e: any) => {
     const form = new FormData();
     form.append("image", e.target.files[0]);
-    const response = await postApi("/api/image", form, false);
+    const response = await postApi(
+      process.env.NEXT_PUBLIC_API_IMAGE_STORE!,
+      form,
+      false
+    );
 
     setImageUrl(response.state.data.url);
   };
@@ -40,11 +44,16 @@ const UploadedLinkRender: React.VFC<Props> = ({ url }) => {
     return <></>;
   }
   return (
-    <NextLink href={url} passHref>
-      <Link isExternal target="_blank">
-        {url}
-      </Link>
-    </NextLink>
+    <Box>
+      <Text fontWeight="bold" fontSize="xl">
+        Access URL
+      </Text>
+      <NextLink href={url} passHref>
+        <Link isExternal target="_blank" color="blue.700">
+          {url}
+        </Link>
+      </NextLink>
+    </Box>
   );
 };
 
